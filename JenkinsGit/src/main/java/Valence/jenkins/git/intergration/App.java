57722623c3e14c072895ac73ac12137ea8e71247
Arjun <Arjun@Arjun-PC>
2015-09-20 16:48:58 +0530

    Initial Commit

---
+++ JenkinsGit/src/main/java/Valence/jenkins/git/intergration/App.java
@@ -0,0 +1,13 @@
+package Valence.jenkins.git.intergration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
