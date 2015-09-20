9c8e9ba2ea736cbd21ae7d2297bcdfc36cd81ca2
Arjun <Arjun@Arjun-PC>
2015-09-20 19:09:45 +0530

    Second commit . Added Display Class As well

--- JenkinsGit/src/main/java/Valence/jenkins/git/intergration/App.java
+++ JenkinsGit/src/main/java/Valence/jenkins/git/intergration/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+       DisplayClass obj = new DisplayClass();
+       obj.display();
     }
 }
---
+++ JenkinsGit/src/main/java/Valence/jenkins/git/intergration/DisplayClass.java
@@ -0,0 +1,10 @@
+package Valence.jenkins.git.intergration;
+
+public class DisplayClass {
+	
+	public void display(){
+		
+		System.out.println("In DisplayClass");
+	}
+
+}
